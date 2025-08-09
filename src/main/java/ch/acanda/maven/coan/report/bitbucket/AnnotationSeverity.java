609d2486af8e026d35d5e33766fe2e0f62ac737f
Philip Graf <git@acanda.ch>
2025-08-09 17:16:15 +0200

    Add support for Bitbucket Code Insights

---
+++ src/main/java/ch/acanda/maven/coan/report/bitbucket/AnnotationSeverity.java
@@ -0,0 +1,5 @@
+package ch.acanda.maven.coan.report.bitbucket;
+
+public enum AnnotationSeverity {
+    CRITICAL, HIGH, MEDIUM, LOW
+}
---
+++ src/main/java/ch/acanda/maven/coan/report/bitbucket/Result.java
@@ -0,0 +1,5 @@
+package ch.acanda.maven.coan.report.bitbucket;
+
+public enum Result {
+    PASSED, FAILED
+}
